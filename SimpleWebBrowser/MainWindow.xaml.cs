197b55fccea5f4c7fcaedb1578f60e28c367824a
natipro22 <natipro22@github.com>
2023-01-20 17:37:19 +0300

    initial commit

--- SimpleWebBrowser/MainWindow.xaml.cs
+++ SimpleWebBrowser/MainWindow.xaml.cs
@@ -24,5 +24,56 @@ namespace SimpleWebBrowser
         {
             InitializeComponent();
         }
+        private void btnGo_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(urlTxtBox.Text) || urlTxtBox.Text.Equals("about:blank"))
+            {
+                MessageBox.Show("enter a valid url");
+                urlTxtBox.Focus();
+                return;
+            }
+
+            openLink(urlTxtBox.Text);
+
+        }
+
+        private void openLink(string url)
+        {
+
+            if (!url.StartsWith("http://") && !url.StartsWith("https://"))
+            {
+                url = "http:\\" + url;
+            }
+            try
+            {
+                webBrowser1.Navigate(new Uri(url));
+            }
+            catch (System.UriFormatException)
+            {
+                return;
+            }
+
+        }
+
+        private void btnBackward_Click(object sender, RoutedEventArgs e)
+        {
+            if (webBrowser1.CanGoBack)
+            {
+                webBrowser1.GoBack();
+            }
+        }
+
+        private void btnForward_Click(object sender, RoutedEventArgs e)
+        {
+            if (webBrowser1.CanGoForward)
+            {
+                webBrowser1.GoForward();
+            }
+        }
+
+        private void btnRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            webBrowser1.Refresh();
+        }
     }
 }
